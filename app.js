7dff1666fa9b980a37fcddc7f52751cef6a1b3b5
Jonathan Rice <jottorice@gmail.com>
2017-12-28 21:41:58 -0600

    try serving the app directory again

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
 //app.use(express.static(__dirname));
 
 app.all('*', function(req, res, next) {
@@ -50,7 +50,7 @@ app.options('*', function(req, res) {
     res.send(200);
 });
 
-//app.use('/', index);
+app.use('/', index);
 app.use('/users', users);
 app.use('/items', items);
 
@@ -65,8 +65,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-//  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.locals.error = err;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
