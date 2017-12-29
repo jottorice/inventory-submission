e2650ea3d855426ce4c69833f1c91636c2e98ef9
Jonathan Rice <jottorice@gmail.com>
2017-12-28 19:18:22 -0600

    Added app path, to try to serve the angular app from there on Heroku

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app')));
 
 app.all('*', function(req, res, next) {
